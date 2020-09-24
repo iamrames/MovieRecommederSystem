06a739ce52297cb3ce66d917922d5d49244f3a30
iamrames <addictedtomig@gmail.com>
2020-09-24 18:57:53 +0545

    Task Complete | task #6

--- flaskr/itemBasedCF.py
+++ flaskr/itemBasedCF.py
@@ -1,37 +1,65 @@
 import pandas as pd
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
 
-r_cols = ['user_id', 'movie_id', 'rating']
-ratings = pd.read_csv([])
+from flaskr.auth import login_required
+from flaskr.db import get_db
 
-m_cols = ['movie_id', 'title']
-movies = pd.read_csv([])
+bp = Blueprint('movies', __name__)
 
-ratings = pd.merge(movies, ratings)
+@bp.route('/movies')
+@login_required
+def index():
+    user_id = g.user
+    if user_id is None:
+        user_id = 0
+    else:
+        user_id = user_id['id']
+    movies = correlate_all_movies(user_id)
+    return render_template('movies/index.html', movies=movies)
 
-userRatings = ratings.pivot_table(index=['user_id'],columns=['title'],values='rating')
 
-corrMatrix = userRatings.corr()
+def correlate_all_movies(user_id):
+    db = get_db()
+    ratings = db.execute(
+        'SELECT user_id,item_id as movie_id,rating FROM user_ratings'
+    ).fetchall()
+    movies = db.execute(
+        'SELECT id as movie_id,title FROM movies'
+    ).fetchall()
+    ratings_df = pd.DataFrame(ratings, columns= ['user_id','movie_id','rating'])
+    movies_df = pd.DataFrame(movies, columns= ['movie_id','title'])
+    ratings = pd.merge(movies_df, ratings_df)
 
-corrMatrix = userRatings.corr(method='pearson', min_periods=100)
+    userRatings = ratings.pivot_table(index=['user_id'],columns=['title'],values='rating')
 
-myRatings = userRatings.loc[0].dropna()
+    corrMatrix = userRatings.corr()
 
-simCandidates = pd.Series()
-for i in range(0, len(myRatings.index)):
-    # "Adding sims for myRatings.index[i]
-    # Retrieve similar movies to this one that I rated
-    sims = corrMatrix[myRatings.index[i]].dropna()
-    # Now scale its similarity by how well I rated this movie
-    sims = sims.map(lambda x: x * myRatings[i])
-    # Add the score to the list of similarity candidates
-    simCandidates = simCandidates.append(sims)
+    corrMatrix = userRatings.corr(method='pearson', min_periods=100)
     
-#Glance at our results so far:
-# "sorting..."
-simCandidates.sort_values(inplace = True, ascending = False)
+    # getting correlated rating by user id
+    myRatings = userRatings.loc[user_id].dropna()
+    
+    simCandidates = pd.Series()
+    for i in range(0, len(myRatings.index)):
+        # "Adding sims for myRatings.index[i]
+        # Retrieve similar movies to this one that I rated
+        sims = corrMatrix[myRatings.index[i]].dropna()
+        # Now scale its similarity by how well I rated this movie
+        sims = sims.map(lambda x: x * myRatings[i])
+        # Add the score to the list of similarity candidates
+        simCandidates = simCandidates.append(sims)
+    
+    #Glance at our results so far:
+    # "sorting..."
+    simCandidates.sort_values(inplace = True, ascending = False)
 
-simCandidates = simCandidates.groupby(simCandidates.index).sum()
+    simCandidates = simCandidates.groupby(simCandidates.index).sum()
 
-simCandidates.sort_values(inplace = True, ascending = False)
+    simCandidates.sort_values(inplace = True, ascending = False)
 
-filteredSims = simCandidates.drop(myRatings.index)
+    filteredSims = simCandidates.drop(myRatings.index,errors = 'ignore')
+
+    return filteredSims
