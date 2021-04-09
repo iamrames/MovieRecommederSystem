fa8bf6a5646d5b78b953d523af5af7c7a2dbffb2
vuown <bhuwanaryal19@gmail.com>
2021-04-09 18:39:21 +0545

    Add changes

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -7,7 +7,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+# bp = Blueprint('auth', __name__, url_prefix='/auth')
+bp = Blueprint('auth', __name__)
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
@@ -39,7 +40,7 @@ def register():
     return render_template('auth/register.html')
 
 
-@bp.route('/login', methods=('GET', 'POST'))
+@bp.route('/', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -58,7 +59,8 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            # return redirect(url_for('index'))
+            return render_template("dashboard.html")
 
         flash(error)
 
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -8,9 +8,10 @@ from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
 
-@bp.route('/')
-def index():
-    db = get_db()
+@bp.route('/posts')
+# def index():
+def view_posts():
+    db = get_db()   
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
